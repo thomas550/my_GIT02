d762fb405534dd8ef9c5b294db8415a545dd0821
BOMLAP-I\Thomas <th.salewsky@web.de>
2018-01-21 09:08:00 +0100

    SSID = xxx

--- platformio_32_OTA_pur/src/main.cpp
+++ platformio_32_OTA_pur/src/main.cpp
@@ -9,8 +9,8 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "xxx";
-const char* password = "xxx";
+const char* ssid = "RegQuadFritz";
+const char* password = "Arabella, die grosse Katze.";
 uint64_t chipid;
 
 //*********************************************************************
